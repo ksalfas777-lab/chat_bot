c46637d7e74feb78fae2d01393a2d544801ac490
MUHAMMED ALFAS KS <ksalfas777@gmail.com>
2026-02-22 15:24:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,77 @@
+import datetime
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from pymongo import MongoClient
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+mongo_url = os.getenv("MONGODB_URI")
+
+client=MongoClient(mongo_url)
+db=client["chat"]
+collection=db["users"]
+
+app = FastAPI()
+
+class ChatRequest(BaseModel):
+    user_id: str
+    question: str
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+
+        ("system", "You are a diet specialist ,Give me the output accordingly"),
+        ("placeholder","{history}"),
+        ("user","{questions}")
+    ]
+)
+llm = ChatGroq(api_key=groq_api_key, model="openai/gpt-oss-20b")
+chain= prompt | llm
+
+
+
+def get_history(user_id):
+    chats=collection.find({"user_id": user_id}).sort("timestamp", 1)
+    history=[]
+
+    for chat in chats:
+        history.append((chat["role"],chat["message"]))
+    return history
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Diet Specialist chatbot API!"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    history = get_history(request.user_id)
+    response = chain.invoke({"history": history, "questions": request.question})
+    collection.insert_one({
+        "user_id": request.user_id,
+        "role": "user",
+        "message": request.question,
+        "timestamp": datetime.utcnow()
+    })
+
+    collection.insert_one({
+        "user_id": request.user_id,
+        "role": "assistant",
+        "message": response.content,
+        "timestamp": datetime.utcnow()
+    })
+
+    return {"response": response.content}
